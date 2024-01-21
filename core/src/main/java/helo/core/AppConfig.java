b01a01f8665596aa8f04f4a9ae40b78281491bc0
Seonghyeon0409 <t139754@gmail.com>
2024-01-21 23:24:24 +0900

    convert spring

--- core/src/main/java/helo/core/AppConfig.java
+++ core/src/main/java/helo/core/AppConfig.java
@@ -6,21 +6,26 @@ import helo.core.discount.RateDiscountPolicy;
 import helo.core.member.*;
 import helo.core.order.OrderService;
 import helo.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository()); // 생성자 주입
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
-
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
--- core/src/main/java/helo/core/MemberApp.java
+++ core/src/main/java/helo/core/MemberApp.java
@@ -4,14 +4,19 @@ import helo.core.member.Grade;
 import helo.core.member.Member;
 import helo.core.member.MemberService;
 import helo.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.net.SocketOption;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
