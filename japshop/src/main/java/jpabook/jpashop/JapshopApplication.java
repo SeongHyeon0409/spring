63d8cc9543922c802989f4f2e89777be60d5aedd
Seonghyeon0409 <t139754@gmail.com>
2024-04-06 23:40:39 +0900

    spring

--- japshop/src/main/java/jpabook/jpashop/JapshopApplication.java
+++ japshop/src/main/java/jpabook/jpashop/JapshopApplication.java
@@ -1,10 +1,17 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JapshopApplication {
+
+	@Bean
+	Hibernate5JakartaModule hibernate5Module() {
+		return new Hibernate5JakartaModule();
+	}
 
 	public static void main(String[] args) {
 
